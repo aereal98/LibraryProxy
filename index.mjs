f5da626cf1aa5a28cf34204f2ec14fd0ab758ecf
aereal98 <121365806+aereal98@users.noreply.github.com>
2023-02-24 16:58:37 -0600

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,54 +1,29 @@
-import createBareServer from '@tomphttp/bare-server-node';
-import { createServer } from 'node:http';
-import { fileURLToPath } from 'node:url';
-import serveStatic from 'serve-static';
-import * as dotenv from 'dotenv'
-dotenv.config()
-const httpServer = createServer();
+import createServer from '@tomphttp/bare-server-node';
+import http from 'http';
+import nodeStatic from 'node-static';
 
 
-// Run the Bare server in the /bare/ namespace. This will prevent conflicts between the static files and the bare server.
-const bareServer = createBareServer('/bare/', {
-	logErrors: false,
-	localAddress: undefined,
-	maintainer: {
-		email: 'tomphttp@sys32.dev',
-		website: 'https://github.com/tomphttp/',
-	},
-});
+const bare =  createServer('/bare/');
+const serve = new nodeStatic.Server('static/');
 
-const serve = serveStatic(
-	fileURLToPath(new URL('./public/', import.meta.url)),
-	{
-		fallthrough: false,
-	}
-);
+const server = http.createServer();
 
-httpServer.on('request', (req, res) => {
-	if (bareServer.shouldRoute(req)) {
-		bareServer.routeRequest(req, res);
+server.on('request', (req, res) => {
+    if (bare.shouldRoute(req)) {
+		bare.routeRequest(req, res);
 	} else {
-		serve(req, res, (err) => {
-			res.writeHead(err?.statusCode || 500, {
-				'Content-Type': 'text/plain',
-			});
-			res.end(err?.stack);
-		});
+		serve.serve(req, res);
 	}
 });
 
-httpServer.on('upgrade', (req, socket, head) => {
-	if (bareServer.shouldRoute(req)) {
-		bareServer.routeUpgrade(req, socket, head);
-	} else {
+server.on('upgrade', (req, socket, head) => {
+	if (bare.shouldRoute(req, socket, head)) {
+		bare.routeUpgrade(req, socket, head);
+	}else{
 		socket.end();
 	}
 });
 
-httpServer.on('listening', () => {
-	console.log('Interstellar running on port 8080');
-});
-
-httpServer.listen({
-	port: process.env.PORT,
+server.listen({
+	port: process.env.PORT || 8080,
 });
